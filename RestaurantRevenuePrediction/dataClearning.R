810b3d8ab94ab32e0c8cf092f6daee507223500c
nianxue <xuenian123@gmail.com>
2015-04-12 14:08:53 -0400

    best earth model

--- RestaurantRevenuePrediction/dataClearning.R
+++ RestaurantRevenuePrediction/dataClearning.R
@@ -4,6 +4,7 @@ library(lubridate)
 library(car)
 library(doParallel)
 library(glmnet)
+library(earth)
 
 setwd("E:/KaggleProject/RestaurantRevenuePrediction")
 trainIni <- read.csv("E:/KaggleProject/RestaurantRevenuePrediction/train.csv/train.csv")
@@ -22,37 +23,40 @@ split <- data$split
 data$Open.Date <- mdy(data$Open.Date)
 data$duration <- (mdy("3/1/2015") - data$Open.Date) / dyears(1)
 
+data <- data %>% group_by(City) %>% mutate(last = (duration == min(duration))) 
+data <- data %>% group_by(City) %>% mutate(first = (duration == max(duration))) 
 
-data$City <- recode(data$City, "'Ä°stanbul' = 'Astanbul'; 'Ankara' = 'Ankara'; else = 'otherCity'")
+
+data$City <- recode(data$City, "'Ä°stanbul' = 'Astanbul'; 'Ankara' = 'Ankara'; 'Ä°zmir' = 'Azmir';else = 'otherCity'")
 data$Type <- recode(data$Type, "'IL' = 'IL'; else = 'FC'")
 
 Pvars <- grep("^P.*\\d$", names(data), value = T)
 correlation <- cor(trainIni[, Pvars], method = "spearman")
-highCorVars <- Pvars[findCorrelation(correlation, cutoff = 0.9)]
+highCorVars <- Pvars[findCorrelation(correlation, cutoff = 0.8)]
 
 keep <- setdiff(names(data), highCorVars)
 data <- data[,keep]
 
 
-data$P1 <- recode(data$P1, "c(1,2) = 2; 3 = 3; 4 = 4; else = 9")
-data$P2 <- recode(data$P2, "c(1, 1.5, 2, 3) = 3; c(4, 4.5) = 4; 5 = 5; else = 7")
-data$P3 <- recode(data$P3, "c(0, 2, 3) = 3; 4 = 4; else = 5")
-data$P4 <- recode(data$P4, "c(2, 3) = 3; c(4, 4.5) = 4; c(6, 7.5) = 6")
-data$P5 <- recode(data$P5, "1 = 1; 2 = 2; else = 4")
-data$P6 <- recode(data$P6, "c(6, 8, 10) = 8")
-data$P7 <- recode(data$P7, "1:4 = 1; c(6, 10) = 10")
-data$P8 <- recode(data$P8, "c(1, 2, 3, 6) = 3")
-data$P10 <- recode(data$P10, "c(8, 10) = 10")
-data$P11 <- recode(data$P11, "c(6, 8, 10) = 10")
-data$P12 <- recode(data$P12, "2:3 = 3; c(6, 8, 10) = 10")
-data$P19 <- recode(data$P19, "c(10, 15, 20, 25) = 10")
-data$P20 <- recode(data$P20, "c(6, 9, 12, 15) = 12")
-data$P21 <- recode(data$P21, "c(4, 5, 6, 9, 12, 15) = 6")
-data$P22 <- recode(data$P22, "c(4, 5, 6, 9, 12, 15) = 6")
-data$P23 <- recode(data$P23, "3:4 = 3; c(10, 15, 20, 25) = 10")
-data$P27 <- recode(data$P27, "c(1, 2, 2.5) = 1; c(3, 4, 5, 7.5, 10, 12.5) = 5")
-data$P28 <- recode(data$P28, "c(2, 2.5) = 2; c(4, 5, 7.5, 10, 12.5) = 5")
-data$P29 <- recode(data$P29, "0:1 = 1; c(2, 2.5) = 2; c(5, 7.5, 10) = 7.5")
+# data$P1 <- recode(data$P1, "c(1,2) = 2; 3 = 3; 4 = 4; else = 9")
+# data$P2 <- recode(data$P2, "c(1, 1.5, 2, 3) = 3; c(4, 4.5) = 4; 5 = 5; else = 7")
+# data$P3 <- recode(data$P3, "c(0, 2, 3) = 3; 4 = 4; else = 5")
+# data$P4 <- recode(data$P4, "c(2, 3) = 3; c(4, 4.5) = 4; c(6, 7.5) = 6")
+# data$P5 <- recode(data$P5, "1 = 1; 2 = 2; else = 4")
+# data$P6 <- recode(data$P6, "c(6, 8, 10) = 8")
+# data$P7 <- recode(data$P7, "1:4 = 1; c(6, 10) = 10")
+# data$P8 <- recode(data$P8, "c(1, 2, 3, 6) = 3")
+# data$P10 <- recode(data$P10, "c(8, 10) = 10")
+# data$P11 <- recode(data$P11, "c(6, 8, 10) = 10")
+# data$P12 <- recode(data$P12, "2:3 = 3; c(6, 8, 10) = 10")
+# data$P19 <- recode(data$P19, "c(10, 15, 20, 25) = 10")
+# data$P20 <- recode(data$P20, "c(6, 9, 12, 15) = 12")
+# data$P21 <- recode(data$P21, "c(4, 5, 6, 9, 12, 15) = 6")
+# data$P22 <- recode(data$P22, "c(4, 5, 6, 9, 12, 15) = 6")
+# data$P23 <- recode(data$P23, "3:4 = 3; c(10, 15, 20, 25) = 10")
+# data$P27 <- recode(data$P27, "c(1, 2, 2.5) = 1; c(3, 4, 5, 7.5, 10, 12.5) = 5")
+# data$P28 <- recode(data$P28, "c(2, 2.5) = 2; c(4, 5, 7.5, 10, 12.5) = 5")
+# data$P29 <- recode(data$P29, "0:1 = 1; c(2, 2.5) = 2; c(5, 7.5, 10) = 7.5")
 
 
 data$Open.Date <- NULL
--- RestaurantRevenuePrediction/linearModel.R
+++ RestaurantRevenuePrediction/linearModel.R
@@ -1,15 +1,10 @@
 load("E:/KaggleProject/RestaurantRevenuePrediction/cleanData.RData")
 
-data[,2:23] <- as_data_frame(lapply(data[,2:23], as.factor))
 data$duration <- log(data$duration)
 data <- model.matrix(~.- Id - 1, data = data)
 
 revenue[revenue > 8.0e+6] = 8.0e+6
 logRevenue <- log(revenue)
-
-correlation <- cor(data[split == 1, ], method = "spearman")
-highCor <- findCorrelation(correlation, cutoff = 0.5)
-data <- data[, -highCor]
 
 train <- data[split == 1, ]
 test <- data[split != 1, ]
@@ -33,37 +28,25 @@ registerDoParallel(cl)
 # set repeatedcv for measure model performace
 trCtrl <- trainControl(method = "repeatedcv", 
                        number = 25, 
-                       repeats = 5, 
+                       repeats = 10, 
                        returnData = FALSE,
                        savePredictions = FALSE)
 
-#set penalty factor for glmnet model
-p.fac = rep(1, dim(train)[2])
-p.fac[43] = 0.01
 
 set.seed(4632)
-glmnet <- train(x = train,
-                y = logRevenue, 
-                method = "glmnet", 
-                family="gaussian",
-                metric = "RMSE", 
-                trControl = trCtrl,
-                tuneGrid = expand.grid(alpha = c(0.1, 0.2, 0.3, 0.35, 0.4, 0.5, 0.6, 0.7), 
-                                       lambda = c(0.05,0.1,0.15,0.2,0.5,10)),
-#                dfmax = 5,
-#                  pmax = 8,
-                 penalty.factor = p.fac
-                )
-glmnetPred <- predict(glmnet, test)
-submission$Prediction <- exp(glmnetPred)
+earth <- train(x = train,
+               y = logRevenue, 
+               method = "earth", 
+               metric = "RMSE", 
+               trControl = trCtrl,
+               tuneGrid = expand.grid(degree = 1, 
+                                       nprune = seq(2,6,1)), 
+               nk = 21,
+               minspan = 20,
+               endspan = 40              
+               )
+pred <- predict(earth, test)
+submission$Prediction <- exp(pred)
 write.csv(submission, "submission.csv", row.names=FALSE)
 
-0.1000  3.0000000  2851666  0.2319482  1115468  0.2203007  0.9
-0.2125  3.0000000  2750951  0.2355501  1142775  0.2187419  0.7
-0.1000  3.0000000  2702419  0.1894374  1091733  0.2164081  0.5
-0.1000  3.0000000  2506590  0.1747579  1107517  0.1924532  0.3
-0.1000  3.0000000  2229493  0.2527332  1162107  0.2526489
-2207934
-revenue[revenue>=8.0e+06] = 8.0e+06
-1964180
 stopCluster(cl)

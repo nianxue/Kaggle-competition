84797f3559ab4be2e11214c43ab21af71bfe57e9
nianxue <xuenian123@gmail.com>
2015-11-16 21:40:49 -0500

    new project

---
+++ RossMannStore/dataClearning.R
@@ -0,0 +1,3 @@
+train <- read.csv("E:/KaggleProject/RossMannStore/train.csv/train.csv", stringsAsFactors=FALSE)
+test <- read.csv("E:/KaggleProject/RossMannStore/test.csv/test.csv", stringsAsFactors=FALSE)
+store <- read.csv("E:/KaggleProject/RossMannStore/store.csv/store.csv")
